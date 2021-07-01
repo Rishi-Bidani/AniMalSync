d8a814511169114fbdfa1df345efc5c736b9233a
Rishi <rbid0001@student.monash.edu>
2021-07-02 01:29:39 +0530

    Initial Commit

---
+++ Anilist.py
@@ -0,0 +1,39 @@
+import requests
+import json
+
+url = 'https://graphql.anilist.co'
+
+query = '''
+query ($id: Int, $page: Int, $perPage: Int, $search: String) {
+    Page (page: $page, perPage: $perPage) {
+        pageInfo {
+            total
+            currentPage
+            lastPage
+            hasNextPage
+            perPage
+        }
+        media (id: $id, search: $search) {
+            id
+            title {
+                english
+                romaji
+            }
+        }
+    }
+}
+'''
+
+
+def searchAnilist(animeName):
+    variables = {
+        'search': animeName,
+        'page': 1,
+        'perPage': 30
+    }
+    response = requests.post(url, json={'query': query, 'variables': variables})
+    result = json.loads(response.text)
+    return result["data"]["Page"]["media"]
+
+
+# print(searchAnilist("naruto"))
